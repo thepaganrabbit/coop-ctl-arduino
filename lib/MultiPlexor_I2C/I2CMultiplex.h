8579e47f4fca9cdcd0236690dfbd356e9373d7b1
thepaganrabbit <gonzalesgustavoc@gmail.com>
2022-11-16 12:44:00 -0700

    feat: updated BMP085 and fixed main.

--- lib/MultiPlexor_I2C/I2CMultiplex.h
+++ lib/MultiPlexor_I2C/I2CMultiplex.h
@@ -2,6 +2,15 @@
 #define I2CMULTIPLEX
 #include <Wire.h>
 #include <SPI.h>
-void TCA9548A(uint8_t bus);
+void TCA9548A(uint8_t bus, bool print = false)
+{
+    Wire.beginTransmission(0x70); // TCA9548A address is 0x70
+    Wire.write(1 << bus);         // send byte to select bus
+    Wire.endTransmission();
+    if (print == true)
+    {
+        Serial.print(bus);
+    }
+}
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,57 +1,27 @@
 #include <Arduino.h>
-#include <Adafruit_BMP085.h>
-#include <Wire.h>
-#include <SPI.h>
 #include <Probe_DS18B20.h>
-#include <SoftwareSerial.h>
-
+#include <I2CMultiplex.h>
+#include <Probe_BMP085.h>
 
 #define probe_bus 2
-#define potentiometer A0
-
 
 ProbeDs18b20 probe_1(probe_bus, 1);
-
-int temp_setting = 0;
-
-void TCA9548A(uint8_t bus){
-  Wire.beginTransmission(0x70);  // TCA9548A address is 0x70
-  Wire.write(1 << bus);          // send byte to select bus
-  Wire.endTransmission();
-  // Serial.print(bus);
-}
+Probe_BMP085 probe_2;
 
 void setup()
 {
   Serial.begin(9600);
   probe_1.intialize_ProbeDs18b20();
    Serial.println("Enter AT Commands");
-  // TCA9548A(0);
-  // if (!bmp.begin())
-  // {
-  //   Serial.println("Could not find a valid BMP085 sensor, check wiring!");
-  // }
-
+  TCA9548A(0);
+  probe_2.initialize();
 }
 
 void loop()
 {
-
-  // temp_setting = analogRead(potentiometer);
-  // TCA9548A(0);
-  // Serial.print("Temperature = ");
-  // float tempC = bmp.readTemperature();
-  // float TempF = 0;
-  // TempF = (tempC*1.8)+32;
-  // Serial.print(bmp.readTemperature());
-  // Serial.println(" *C");
-  // Serial.println();
-  // Serial.print("Temperature = ");
-  // Serial.print(TempF);
-  // Serial.println(" *F");
-  // Serial.println();
-  // probe_1.print();
-  // Serial.println("-----------");
-  // Serial.println(temp_setting);
-  // delay(500);
+  TCA9548A(0);
+  probe_1.print();
+  delay(1000);
+  probe_2.print();
+  delay(500);
 }
